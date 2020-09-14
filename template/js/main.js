8b062dd0485094f647ac530db4c62671cd0fa99d
QD <datnguyenquoc@admicro.vn>
2020-09-14 18:05:20 +0700

    14/9

---
+++ template/js/main.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 100,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
